85bc2cda1b213483be09882776283ffff439d376
evo-brut3 <kale2134+1@gmail.com>
2021-02-17 11:19:03 +0100

    Login Page now displays when user is not signed  - add Provider dependency  - add shared_preferences dependency  -  move MyHomePage to home_view.dart  - now RootView is main view in MaterialApp  - add signOut() in AuthFirebase  - add sign out button

---
+++ lib/views/home_view.dart
@@ -0,0 +1,95 @@
+import 'package:fdnt/views/drawer_view.dart';
+import 'package:fdnt/views/login_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Center(
+      child: Column(
+        children: [SignInForm()],
+      ),
+    ),
+    Text(
+      'Index 1: Communicator',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Mails',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Calendar',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Info',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FDNT"),
+      ),
+      body: Center(
+          child: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey[500],
+        showUnselectedLabels: true,
+        selectedFontSize: 8,
+        unselectedFontSize: 8,
+        iconSize: 30,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Główna',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Komunikator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mail),
+            label: "Poczta",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: "Kalendarz",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: "O Fundacji",
+          ),
+        ],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
+      drawer: DrawerView(),
+// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,71 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fdnt/services/firebase_auth.dart';
 
-class LoginView extends StatelessWidget {
+class LoginView extends StatefulWidget {
+  LoginView({Key key}) : super(key: key);
+
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  int _selectedIndex = 0;
+
+  static List<Widget> _widgetOptions;
+
+  void initState() {
+    _widgetOptions = <Widget>[
+      Text('Index 1: Home'),
+      SignInForm(),
+      Text('Index 3: Info'),
+    ];
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FDNT"),
+      ),
+      body: Center(
+          child: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey[500],
+        showUnselectedLabels: true,
+        selectedFontSize: 8,
+        unselectedFontSize: 8,
+        iconSize: 30,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Główna',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.lock),
+            label: "Zaloguj się",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.info),
+            label: "O Fundacji",
+          ),
+        ],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
+    );
+  }
+}
+
+class SignInForm extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
@@ -63,6 +127,22 @@ class LoginView extends StatelessWidget {
                 ),
               )),
           padding: const EdgeInsets.fromLTRB(16, 8, 16, 16),
+        ),
+        Container(
+          child: ElevatedButton(
+              onPressed: () {
+                AuthFirebase().signOut();
+              },
+              child: Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(0, 12, 0, 12),
+                child: Text(
+                  "Wyloguj się",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+              )),
+          padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
         )
       ],
     );
